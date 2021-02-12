7a667bc03e10d50717a97876287990cbec85466f
Andrea Patricelli <andreapatricelli@apache.org>
2021-02-12 15:48:16 +0100

    closes #12

--- app/src/main/java/org/apache/syncope/client/enduser/panels/any/UserDetails.java
+++ app/src/main/java/org/apache/syncope/client/enduser/panels/any/UserDetails.java
@@ -99,6 +99,7 @@ public class UserDetails extends Details<UserTO> {
             add(new PasswordPanel(
                     "passwordPanel",
                     wrapper,
+                    wrapper.getInnerObject().getKey() == null,
                     templateMode,
                     new PasswordStrengthBehavior(new PasswordStrengthConfig().
                             withDebug(false).
